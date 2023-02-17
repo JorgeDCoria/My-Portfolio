9022b3b7cd950e7d0baca828a2cfde4dd386efd3
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-02-17 00:16:07 -0300

    feat/ add button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+export default function Button({ onClick, text, danger, md, lg }) {
+  useEffect(() => {
+    if (!md && !lg)
+      throw new Error("not found size md or lg <ButtonMd md .../>");
+    if (md && lg)
+      throw new Error("you can't select both properties md && lg, choose one ");
+  }, []);
+  return (
+    <>
+      <button
+        className={`${
+          danger
+            ? "bg-red-500 hover:bg-red-400"
+            : "bg-secondary_400 hover:bg-secondary_300"
+        } ${md && " w-24"}  ${lg && "w-44"} text-md
+        h-10 rounded-2xl font-bold text-white shadow-md shadow-primary_800`}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </>
+  );
+}
