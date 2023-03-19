9113bfd2f6b787229dec53471615e1b606852b10
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-03-19 01:02:26 -0300

    perf/ about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,33 +1,35 @@
 import React from "react";
-import perfil from "../../assets/perfil.jpeg";
+import aboutme from "../../assets/aboutme.jpg";
 const About = () => {
   return (
-    <div className="flex h-[80vh] w-full flex-col items-center justify-around md:h-[90vh] lg:flex-row">
-      <div className="flex h-40 w-full items-center justify-around rounded-lg bg-neutral_50 p-4 md:h-56 md:w-3/4 lg:h-[450px] lg:w-2/6 lg:flex-col ">
-        <h2 className="font-subtitle text-3xl text-primary_500">¿Quien soy?</h2>
+    <div className="box-border flex min-h-[80vh] w-full flex-col items-center justify-around overflow-y-auto bg-gradient-to-b from-primary_900 to-primary_700 p-4 lg:flex-row">
+      <div className="flex h-40 w-full items-center justify-center rounded-lg p-4 md:min-h-[300px] md:w-3/4 lg:h-full lg:w-1/2">
         <img
-          src={perfil}
-          className=" h-24 w-24 rounded-2xl object-cover shadow-xl shadow-primary_700 md:h-40 md:w-40 lg:h-80 lg:w-80"
+          src={aboutme}
+          className=" object-content w-full rounded-2xl shadow-xl shadow-secondary_base md:h-full lg:h-80  lg:w-[80%] lg:-rotate-45 "
           alt=""
         />
       </div>
 
-      <div className="flex w-full items-center justify-center rounded-lg bg-primary_700 p-4 md:w-3/4 lg:h-[450px] lg:w-[70ch]">
-        <p className="overflow-y-auto text-justify text-sm text-primary_50 md:text-lg lg:text-base ">
-          Hello world! Soy Coria Jorge, un estudiante avanzado en en la carrera
-          Analista Programador Universitario de la Universidad Nacional de Jujuy
-          (pendiente una materia) y graduado como Full Stack Developer del
-          Bootcamp Soy Henrry. <br /> En el proceso de mi formación eh realizado
-          proyectos de forma individual como grupal utilizando diversos
-          lenguajes y herramientas tecnologicas como Java, SpringBoot, Angular,
-          TypeScript, C#, .Net, Bootstrap entre otros. Actualmente estoy en mas
-          contacto con React, Redux, Tailwind, Express, Node y sequelize. <br />{" "}
-          Soy una persona curiosa y con entusiasmo por aprender siempre cosas
-          nuevas. Me encanta ser parte de equipos de trabajo sumamente diversos
-          y que con nuestras diferentes culturas nos podamos enriquecer. Creo
-          que la diversidad de historias, culturas y pensamientos hacen a los
-          grandes equipos. Si todos pensáramos igual, ¿Cómo le daríamos lugar a
-          la creatividad?
+      <div className="flex w-full items-center justify-center rounded-lg p-4 lg:w-1/2">
+        <p className="overflow-y-auto text-justify text-sm text-white md:w-[60ch] md:text-lg ">
+          Hello world! Mi nombre es Coria Jorge un apasionado por la tecnologia
+          y el desarrollo de software.
+          <br />
+          Desde joven, sempre eh estado interesado en la informatica y en como
+          funciona el software. En la Universidad, decidi seguir mi pasion y
+          estudiar la carrera Analista Programador Universitario, a finales de
+          la carrera tambien ingrese a un bootcamp para obtener el titulo de
+          Full Stack Developer. En el proceso de mi formación aprendi
+          habilidades fundamentales que necesitaba para construir soluciones de
+          software, como lenguajes de programacion como React, Redux, Tailwind,
+          Express, Node, Java, SpringBoot, Angular, TypeScript, C#, .Net,
+          Bootstrap, bases de datos, deseño de interfaces de usuario, gestion de
+          proyectos y el trabajo en equipo usando metodologias agiles. <br />
+          Si estas buscando un programador apasionado, trabajador y creativo,
+          creo que pordriamos ser una buena opcion. Me encantaria tener la
+          oportunidad de trabajar contigo y ayudarte a construir soluciones de
+          software innovadores y de alta calidad.
         </p>
       </div>
     </div>
